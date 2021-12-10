2a0bc799c017368ab1599c56ebcfeb0799d4038e
Siddhant Kumar <siddhantkumarupmanyu@gmail.com>
2021-12-11 01:05:58 +0530

    update scratchpad.txt

--- app/src/androidTest/java/sku/challenge/atmanatodoapp/EndToEndTest.kt
+++ app/src/androidTest/java/sku/challenge/atmanatodoapp/EndToEndTest.kt
@@ -4,12 +4,14 @@ import org.junit.Test
 
 class EndToEndTest {
 
+    // back navigation is working correctly -> I should consider another UI related,
+    //  I do not think it has anything functionality apart from UI
     @Test
-    fun localTodos(){
-        // 1. if I am able to add a TODO
-        // 2. if I back navigation is working correctly -> I should consider another UI related,
-        //      I do not think it has anything functionality apart from UI
-        // 3. if I am able to delete a TODO
+    fun localTodos() {
+        // insert a item
+        // edit that item
+        // delete that item
+
     }
 
 }
