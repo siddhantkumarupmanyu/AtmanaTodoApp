65084e8d7d68bac2d03d586cd75846456c2eddb5
Siddhant Kumar <siddhantkumarupmanyu@gmail.com>
2021-12-11 01:02:18 +0530

    add EndToEndTest.kt file
    no test in there

---
+++ app/src/androidTest/java/sku/challenge/atmanatodoapp/EndToEndTest.kt
@@ -0,0 +1,15 @@
+package sku.challenge.atmanatodoapp
+
+import org.junit.Test
+
+class EndToEndTest {
+
+    @Test
+    fun localTodos(){
+        // 1. if I am able to add a TODO
+        // 2. if I back navigation is working correctly -> I should consider another UI related,
+        //      I do not think it has anything functionality apart from UI
+        // 3. if I am able to delete a TODO
+    }
+
+}
