1844e8c7d201ffe5c088715ed56583cd3f205f90
Siddhant Kumar <siddhantkumarupmanyu@gmail.com>
2021-12-11 08:56:55 +0530

    add RemoteViewModel.kt

---
+++ app/src/main/java/sku/challenge/atmanatodoapp/ui/RemoteViewModel.kt
@@ -0,0 +1,11 @@
+package sku.challenge.atmanatodoapp.ui
+
+import androidx.lifecycle.ViewModel
+import sku.challenge.atmanatodoapp.repository.ItemRepository
+
+class RemoteViewModel(
+    itemRepository: ItemRepository
+) : ViewModel() {
+
+
+}
